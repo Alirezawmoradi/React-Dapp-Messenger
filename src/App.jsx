92584e6d4c456bd93a0084fa31f160b87d0e41cf
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-27 22:06:09 +0330

    tailwind added to project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div
+            className="flex justify-center items-center w-screen h-screen bg-gray-800 text-2xl font-extrabold text-white">Crypto
+            React Dapp Messenger
+        </div>
+    )
+}
+
+export default App

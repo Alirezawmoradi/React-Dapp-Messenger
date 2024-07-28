a3cabe682b395e324182529e10d47030c2eaed54
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-28 15:45:44 +0330

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import {Messenger} from "./feature/messenger.jsx";
+
 function App() {
     return (
         <div
-            className="flex justify-center items-center w-screen h-screen bg-gray-800 text-2xl font-extrabold text-white">
-            React Dapp Messenger
+            className="flex justify-center items-center w-screen h-screen bg-gray-800 text-base font-extrabold text-white">
+            <Messenger/>
         </div>
     )
 }

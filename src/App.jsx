fe0bb7818991e5ad1ec70c650757861ea56247f1
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-29 15:58:41 +0330

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import {Messenger} from "./feature/messenger.jsx";
+import { Messenger } from "./feature/messenger.jsx";
 
 function App() {
     return (
-        <div
-            className="flex justify-center items-center w-screen h-screen bg-gray-800 text-base font-extrabold text-white">
-            <Messenger/>
+        <div className="flex justify-center items-center w-screen h-screen bg-gray-900 text-base font-medium text-white">
+            <Messenger />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
